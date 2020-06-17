46cb211fd6465a5a8a246d46224154c3105145a5
남욱 김 <knugo@naver.com>
2020-06-17 14:55:52 +0900

    test

---
+++ HelloSolution/HelloTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Hello;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World!", Program.CreateMassage01());
+        }
+    }
+}
